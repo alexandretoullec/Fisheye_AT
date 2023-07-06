418cb633eecbd35e4c37b691ca3d6844346077ae
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-07-06 11:56:47 +0200

    first commit

---
+++ scripts/pages/photographer.js
@@ -0,0 +1,39 @@
+//Mettre le code JavaScript lié à la page photographer.html
+
+// récupérer l'ID du photographe dans l'URL et on retourne les datas
+async function getPhotographersById() {
+  const reponse = await fetch("data/photographers.json");
+  const datas = await reponse.json();
+  const photographers = datas.photographers;
+
+  const params = new URL(document.location).searchParams;
+  const id = parseInt(params.get("id"));
+
+  const photographer = photographers.find(
+    (photographer) => photographer.id === id
+  );
+
+  return photographer;
+}
+
+console.log(getPhotographersById());
+
+let factory = null;
+
+async function init() {
+  const photographer = await getPhotographersById();
+  console.log(photographer);
+  factory = photographerTemplate(photographer);
+  await renderHeader();
+}
+
+async function renderHeader() {
+  //   const mainCont = document.querySelector("#main");
+  //   const photograpHeader = document.createElement("section");
+  //   mainCont.prepend(photograpHeader);
+  const mainCont = document.querySelector(".photograph-header");
+
+  mainCont.innerHTML = factory.getUserHeader();
+}
+
+init();
---
+++ scripts/templates/photographer.js
@@ -0,0 +1,69 @@
+//factory function simplified version of classes
+
+function photographerTemplate(data) {
+  //destructuring objects from data
+  const { id, name, portrait, city, country, tagline, price } = data;
+
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    //creation de l'article
+    const article = document.createElement("article");
+
+    //creation de l'anchor tag lien vers la page du photographe
+    const containerLink = document.createElement("a");
+    containerLink.setAttribute("href", `./photographers.html?id=${id}`);
+    containerLink.setAttribute("aria-label", `lien vers la page de ${name}`);
+
+    //creation de l'image
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", `photo de ${name}`);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    //creation de la location
+    const location = document.createElement("h3");
+    location.innerText = `${city} , ${country}`;
+
+    // creation de la tagline
+    const tagLine = document.createElement("h4");
+    tagLine.innerText = tagline;
+
+    // creation du prix
+    const priceData = document.createElement("p");
+    priceData.textContent = `${price}€/jour`;
+
+    //affichage et hierarchisation dans le DOM
+    article.appendChild(containerLink);
+    containerLink.appendChild(img);
+    containerLink.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(tagLine);
+    article.appendChild(priceData);
+
+    return article;
+  }
+
+  function getUserHeader() {
+    const headerPhotographer = `
+      <div class="photograph-header-descr">
+        <h2>${name}</h2>
+        <h3> ${city} , ${country}</h3>
+        <h4>${tagline}</h4>
+      </div>
+
+      <button class="contact_button" onclick="displayModal()">
+      Contactez-moi
+      </button>
+      
+      <img src=${picture} alt=photo de ${name}>
+      
+    
+  `;
+
+    return headerPhotographer;
+  }
+
+  return { name, picture, getUserCardDOM, getUserHeader };
+}
