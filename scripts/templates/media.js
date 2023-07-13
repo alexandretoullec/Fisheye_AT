b0332867f7a4b427686ddee531aeb4edaf81e2e5
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-07-13 12:30:38 +0200

    last modif

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,15 +1,28 @@
 function mediaTemplate(data) {
-  const { id, photographerId, title, image, likes, date, price } = data;
+  const { id, photographerId, title, image, likes, video, date, price } = data;
 
   const picture = `assets/images/${image}`;
-
+  const videoLink = `assets/images/${video}`;
   function getMedia() {
     //creation de l'article
     const article = document.createElement("article");
     article.classList.add("media-img-card");
     //creation de l'img
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
+    let img = "";
+
+    if (image) {
+      img = document.createElement("img");
+      img.setAttribute("src", picture);
+    } else if (video) {
+      img = document.createElement("video");
+
+      // img.setAttribute("controls");
+      img.setAttribute("width", 250);
+      img.setAttribute("height", 250);
+      const source = document.createElement("source");
+      source.setAttribute("src", videoLink);
+      img.append(source);
+    }
 
     //creation du container texte
     const textContainer = document.createElement("div");
