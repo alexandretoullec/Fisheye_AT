560d6dc76ea33cd416fa4867e9b7ad9758340d44
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-08-08 13:01:39 +0200

    changes

--- scripts/factories/dataFactory.js
+++ scripts/factories/dataFactory.js
@@ -1,8 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 class DataFactory {
   constructor(data, type) {
     if (type === "video") {
+      // eslint-disable-next-line no-undef
       return new VideoTemplate(data);
     } else if (type === "photo") {
+      // eslint-disable-next-line no-undef
       return new PhotoTemplate(data);
     } else {
       throw "unknow type format";
